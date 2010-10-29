60c97f873dee6d2ba5f8742fde2aa507cd345907
Freddy Andersen <freddy@zerolimitstech.com>
2010-10-28 23:01:50 -0700

    typo

--- app/portlets/twitter_user_timeline_portlet.rb
+++ app/portlets/twitter_user_timeline_portlet.rb
@@ -6,7 +6,7 @@ class TwitterUserTimelinePortlet < Portlet
   def render
     raise ArgumentError, "No Twitter Username was given" if self.username.blank?
 
-    @items = Timline.find_or_create_by_username(self.username).get_timeline(@portlet.limit)
+    @items = Timeline.find_or_create_by_username(self.username).get_timeline(@portlet.limit)
 
     instance_eval(self.code) unless self.code.blank?
   end
